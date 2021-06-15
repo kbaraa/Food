a26020b38ad3aec3c192de35cff974d0ad906c14
kbaraa <coding.kent@gmail.com>
2021-06-15 16:10:26 +0900

    firebase update

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 
 import HeaderCartButton from './HeaderCartButton';
-import mealsImage from '../../assets/meals.jpg';
+
 import classes from './Header.module.css';
 
 const Header = (props) => {
@@ -11,9 +11,7 @@ const Header = (props) => {
         <h1>Kent's Foods</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
-      <div className={classes['main-image']}>
-        <img src={mealsImage} alt='A table full of delicious food!' />
-      </div>
+
     </Fragment>
   );
 };
